09fb429264f8c60488d9ede1d09eba27168daf55
Alex Galicia <gg14054@ues.edu.sv>
2025-09-11 02:37:40 -0600

    Initial commit with API and Front starting boilerplates

---
+++ minilms-api/src/main/java/com/minilms/api/MinilmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.minilms.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinilmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinilmsApiApplication.class, args);
+	}
+
+}
