83658bf62d7954c346393369c137bfea5a7352f1
JosueCLopez <cl22006@ues.edu.sv>
2025-10-28 23:10:22 -0600

    feat(routing): agrega la ruta para el componente de registro

--- edubyte-frontend/src/app/app.routes.ts
+++ edubyte-frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 import { LoginComponent } from './auth/login/login.component';
-
+import { RegisterComponent } from './auth/register/register.component';
 
 export const routes: Routes = [
     {
@@ -11,5 +11,8 @@ export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
-    }
+    },
+    { 
+        path: 'register', 
+        component: RegisterComponent },
 ];
